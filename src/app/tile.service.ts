87882b8c0f84a9fa690b6dbbee17c02c096d3336
hozzj <medo3578@gmail.com>
2017-04-19 11:01:27 +0200

    added strict mode and some sort of winning indication needs more work though

--- src/app/tile.service.ts
+++ src/app/tile.service.ts
@@ -7,42 +7,57 @@ import { repeat, handleUserEntries } from "./util"
 @Injectable()
 export class TileService {
   public winningCount: number = 0
+  private strict: boolean = false
   private PCMemory: number[] = []
   private userMemory: number[] = []
   private userTurn: boolean = false
   private audioEl: HTMLAudioElement
   private controls: TileComponent[] = []
 
+  // generates a random tile index
   private generate() {
     this.PCMemory.push(Math.round(Math.random() * 3))
   }
-  private repeat() {
+  // should repeat the last tiles in memory for the user
+  // delegates control to util.ts
+  private repeat(): void {
     repeat(this)
   }
+  public strictSwitch() {
+    this.strict = !this.strict
+  }
+  // this should handle user's entries
   public handleClick(tile: Tile): void {
     handleUserEntries(this, tile)
   }
-  public startGame() {
+  // this should keep the game going
+  // it generates a new index
+  // then repeats the entries to the user
+  // and gives control back to the beloved user
+  public startGame(): void {
     if (!this.userTurn) {
       this.generate()
       this.repeat()
       this.userTurn = true
     }
   }
+  // this should reset everything at any time
+  // memories are purged, score reset, control back to PC
   public reset(): void {
     this.winningCount = 0
     this.PCMemory = []
     this.userMemory = []
     this.userTurn = false
   }
-  // mundane oo stuff to keep encapsulation intact
-  // I fucking hate oop
+  // should take TileComponents to control them from within the service
   public addControls(control: TileComponent) {
     this.controls.push(control)
   }
+  // to keep encapsulation intact
   public audioElAdd(audio: HTMLAudioElement) {
     this.audioEl = audio
   }
+  // to just have one audio element is better
   public play(sound: string) {
     this.audioEl.src = sound
     this.audioEl.play()
--- src/app/util.ts
+++ src/app/util.ts
@@ -1,27 +1,47 @@
 import { Tile } from "./tile.model";
-// REEEEEEEEEEEEEEEEEEEEEEEFACTOOOOOOOOOOOOOOOOOOR !!!!!!!
+import { TileComponent } from "./tile/tile.component";
 
-export const repeat = (that: any) => {
+export const repeat = (that: any): void => {
   let i = 0
+
+  // this should go ahead an toggle between the colors of mousedown and mouseup
+  const toggleColor = (): TileComponent => that.controls[that.PCMemory[i]].changeColor()
+
+  // this should break the loop of the interval
+  // TODO: find something better than intervals
+  // they're messy and all shitty
   const breakTheloop = (): void => {
     i = 0
     that.userTurn = true
     clearInterval(interv)
   }
-  const toggleColor = () => that.controls[that.PCMemory[i]].changeColor()
+
+  // this should be used in a set timeout 
+  // to change back to normal color 
+  // to imitate an animation effect 
+  const toggleBack = (): void => {
+    toggleColor()
+    i += 1
+    if (i >= that.PCMemory.length)
+      breakTheloop()
+  }
+
+  // changes color then plays the tile's sound
+  // TileComponent's funcs return their 'this' to ease chaining
   const animate = (): void => {
     toggleColor().play()
-    setTimeout(() => {
-      toggleColor()
-      i += 1
-      if (i >= that.PCMemory.length)
-        breakTheloop()
-    }, 200)
+    setTimeout(toggleBack, 200)
   }
-  let interv = setInterval(animate, 800)
+
+  // all comes to this pos: PieceOfShit
+  // 800 ms for the animation effect and the sound to play
+  const interv = setInterval(animate, 800)
 }
+
 export const handleUserEntries = (that: any, tile: Tile) => {
-  const keepGoing = () => {
+  // this works if all of the user's entries are correct
+  // user then is given a score increase of 1
+  const keepGoing = (): void => {
     that.winningCount += 1
     that.startGame()
   }
@@ -41,8 +61,13 @@ export const handleUserEntries = (that: any, tile: Tile) => {
         that.userMemory = []
       }
     } else {
-      that.repeat()
-      that.userMemory = []
+      if (that.strict) {
+        that.reset()
+        that.startGame()
+      } else {
+        that.repeat()
+        that.userMemory = []
+      }
     }
   }
 }
